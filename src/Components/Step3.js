83c42286ecfbf1228f30f29f801c93fe3081d8c1
samreen675 <30996450+samreen675@users.noreply.github.com>
2022-08-17 13:39:26 +0530

    Update Step3.js

--- src/Components/Step3.js
+++ src/Components/Step3.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import { DataContext } from "../App";
 
 const Step1 = (props) => {
